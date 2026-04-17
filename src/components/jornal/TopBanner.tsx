99b56364b414f11902fb44218b14c9c442575bd6
jornalmarabasite-alt <jornalmarabasite@gmail.com>
2026-04-17 15:55:55 -0300

    Update TopBanner.tsx

--- src/components/jornal/TopBanner.tsx
+++ src/components/jornal/TopBanner.tsx
@@ -1,7 +1,7 @@
 export function TopBanner() {
   return (
     <div className="mt-3">
-      <div className="bg-jm-banner flex items-center justify-center text-white/90 font-serif italic text-xl sm:text-3xl text-center px-6 aspect-[16/10] sm:aspect-[16/5]">
+      <div className="bg-jm-banner flex items-center justify-center text-white/90 font-serif text-xl sm:text-3xl text-center px-6 aspect-[16/10] sm:aspect-[16/5]">
         Espaço para propaganda
         <br />
         do banner do topo
